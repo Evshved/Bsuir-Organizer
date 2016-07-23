011a1279116b8a169763967a460af003b1b1a512
Shved <snuggleshved@gmail.com>
2016-07-23 20:23:43 +0300

    update scrapper

--- scrapper/parser.rb
+++ scrapper/parser.rb
@@ -1,37 +1,59 @@
-require 'rubygems'
 require 'roo'
 
 
 class Work
-  @path = "./Raspisanie_zanyatiy_dlya_studentov_BGUIR_242701.xlsx"
-  @tablesheet = Roo::Spreadsheet.open(@path)
-  def second_and_forth_weeks
-    @counter = 1
-    @tablesheet = 39
-    while @counter < @tablesheet do #.last_row
-      @check_week = @tablesheet.cell('B', @counter)
-      if @check_week == "1, 3"
-        @time = tablesheet.cell('C', @counter)
-        @subject = tablesheet.cell('D', @counter)
-        @type_of_subject = tablesheet.cell('E', @counter)
-        @number_of_subgroup = tablesheet.cell('F', @counter)
-        @lecture_hall = tablesheet.cell('G', @counter)
-        @teacher  = tablesheet.cell('H', @counter)
-        @note = tablesheet.cell('I', @counter)
-        puts (" #{@time}  #{@subject} #{@type_of_subject} #{@number_of_subgroup}
-        #{@lecture_hall} #{@teacher} #{@note}")
-      end
-      @counter +=1
-    end
+  def initialize
+    @path = "./Raspisanie_zanyatiy_dlya_studentov_BGUIR_242701.xlsx"
+    @tablesheet = Roo::Spreadsheet.open(@path)
+    @hash_of_params =  {1 => "#{@time}", 2 => "#{@subject}",
+      3 => "#{@type_of_subject}", 4 => "#{@number_of_subgroup}",
+      5 => "#{@lecture_hall}", 6 => "#{@teacher}", 7 =>"#{@note}" }
+    @number_of_hash_params = @hash_of_params.length
+    @index_of_row = 4
+    @max_index_of_row = @tablesheet.last_row
 
   end
+
+  def parsing_loop( number_of_week )
+    @number_of_week = number_of_week
+    while @index_of_row < @max_index_of_row do
+      @check_week = @tablesheet.cell( 'B', @index_of_row )
+      check
+      @index_of_row +=1
+    end
+  end
+
+  def check
+    @counter_of_hash = 1
+    if @check_week.to_s.include? "#{@number_of_week}"
+      ( "C".."I" ).each do |index_of_column|
+        @hash_of_params[@counter_of_hash] = @tablesheet.cell( index_of_column,
+        @index_of_row )
+        @counter_of_hash += 1
+      end
+      output
+    end
+  end
+
+  def output
+    ( 1..@number_of_hash_params ).each do | key_of_hash |
+      puts( @hash_of_params[key_of_hash] )
+    end
+  end
+
+
 end
 
 
-job = Work.new
-puts("1&3 weeks")
-job.second_and_forth_weeks
+# job = Work.new
+# puts("1 week  ===================","")
+# job.parsing_loop("1")
 
+(1..4).each do |counter|
+  job = Work.new
+  puts("#{counter} week  ===================","")
+  job.parsing_loop("#{counter}")
+end
 
 # Oga.parse_xml(handle)
 
@@ -41,7 +63,10 @@ job.second_and_forth_weeks
 #   end
 # end
 
-
+  # def parsing(gobelen)
+  #   @gobelen = gobelen
+  #   puts (@gobelen)
+  # end
 
 # def second_and_forth_weeks
 #  if check_week == "2, 4"
