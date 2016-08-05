a959683516e19ddcdf0996c34e7f37b77e4f0813
Shved <snuggleshved@gmail.com>
2016-08-06 02:26:42 +0300

    Add capybara. Add tests for log in exist account.

--- spec/tests_for_users/log_in.rb
+++ spec/tests_for_users/log_in.rb
@@ -11,4 +11,28 @@ feature 'Log in' do
     expect(page).to have_content("Instructions here.")
   end
 
+  scenario 'log in another exist account' do
+    visit '/'
+    fill_in 'Email', with: "kate@gmail.com"
+    fill_in 'Password', with: "112233"
+    click_button 'Log in'
+
+    expect(page).to have_content("Instructions here.")
+  end
+
+  scenario 'dual log in' do
+    visit '/'
+    fill_in 'Email', with: "kate@gmail.com"
+    fill_in 'Password', with: "112233"
+    click_button 'Log in'
+    expect(page).to have_content("Instructions here.")
+
+    click_link 'Logout'
+    fill_in 'Email', with: "aaa@gmail.com"
+    fill_in 'Password', with: "112233"
+    click_button 'Log in'
+
+    expect(page).to have_content("Instructions here.")
+  end
+
 end
