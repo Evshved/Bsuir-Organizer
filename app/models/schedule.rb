966360d7ce60034b7b9d1aa98af38235da41008f
Shved <snuggleshved@gmail.com>
2016-12-23 23:37:49 +0300

    Update models

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,14 @@
 class Schedule < ApplicationRecord
-  belongs_to :user
-  validates :teacher, presence: true, teacher: true
-  validates :schedule, presence: true, teacher: true
-  validates :id_of_week, presence: true, id_of_week: true
-  validates :weekday, presence: true, weekday: true
+  belongs_to :user, foreign_key: 'user_id', optional: true
+
+  validates :id_of_week, length: { is: 1 }
+  validates :weekday, length: { minimum: 3, maximum: 12 }
+  validates :start_time_class, presence: true, schedule: true
+  validates :end_time_class,   presence: true, schedule: true
+  validates :name_of_subject, length: { minimum: 2, maximum: 15 }
+  validates :type_of_subject, length: { maximum: 2 }
+  validates :id_of_subgroup, length: { is: 1 }
+  validates :number_of_audience, length: { maximum: 11 }
+  validates :note, length: { maximum: 65 }
+  validates :teacher, length: { minimum: 3, maximum: 65 }
 end
