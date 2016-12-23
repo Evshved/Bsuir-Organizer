f1969ab5cfc6378622cd5e415fd85c8b61743858
Shved <snuggleshved@gmail.com>
2016-12-23 23:36:55 +0300

    Add validator for Schedule

--- app/validators/schedule_validator.rb
+++ app/validators/schedule_validator.rb
@@ -1,2 +1,15 @@
 class ScheduleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    check = value.to_s.include? ':'
+    if check
+      interval_time(record, attribute, value)
+    end
+  end
+
+  def interval_time(record, attribute, value)
+    value = value.split(':')
+    value.map!(&:to_i)
+    return true if ((0..23).cover? value[0]) && ((0..59).cover? value[1])
+    record.errors[attribute] << (options[:message] || 'Field is incorrect')
+  end
 end
