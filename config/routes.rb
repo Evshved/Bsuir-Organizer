f0989a4f4695e307cee6419e9e7884a4c374149e
Shved <snuggleshved@gmail.com>
2017-01-03 00:14:04 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :records
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :instructions
+  resources :instructions, :schedules
   root 'instructions#index'
   get  "instructions/new"   => "instructions#new"
   post "instructions/new"   => "instructions#create"
