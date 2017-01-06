95ffe97967ee438b7f08df3f889a1db36c0e574d
Shved <snuggleshved@gmail.com>
2017-01-07 02:26:18 +0300

    Delete react trash files

--- app/assets/javascripts/components/schedule.js
+++
@@ -1,5 +0,0 @@
-this.Schedule = React.createClass({
-  render: function() {
-    return React.DOM.tr(null, React.DOM.td(null, this.props.schedule.end_time_class), React.DOM.td(null, this.props.schedule.start_time_class), React.DOM.td(null, amountFormat(this.props.schedule.id_of_week)));
-  }
-});
