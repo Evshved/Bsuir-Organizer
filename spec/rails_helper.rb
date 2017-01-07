23df45ebff1a3e349c9642152c1faf1b73205214
Shved <snuggleshved@gmail.com>
2017-01-07 04:12:40 +0300

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+env = 'The Rails environment is running in production mode!'
+abort(env.to_s) if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 
@@ -13,7 +14,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 end
 
-require "shoulda/matchers"
+require 'shoulda/matchers'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
