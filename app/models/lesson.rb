a8e66b120c6e4ce70311f9efb2cc6030f95792fe
Shved <snuggleshved@gmail.com>
2017-01-19 00:31:34 +0300

    Update validates && add new models

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,4 @@
-class Schedule < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id', optional: true
-
+class Lesson < ApplicationRecord
   validates :id_of_week, length: { is: 1 }
   validates :weekday, length: { minimum: 3, maximum: 12 }
   validates :start_time_class, presence: true, schedule: true
@@ -8,7 +6,8 @@ class Schedule < ApplicationRecord
   validates :name_of_subject, length: { minimum: 2, maximum: 15 }
   validates :type_of_subject, length: { maximum: 2 }
   validates :id_of_subgroup, length: { is: 1 }
+  validates :id_of_group, length: { is: 6 }
   validates :number_of_audience, length: { maximum: 11 }
   validates :note, length: { maximum: 65 }
-  validates :teacher, length: { maximum: 65 }
+  validates :teacher, length: {maximum: 65 }
 end
